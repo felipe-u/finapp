e3cd71935f87bd6dee422cb377e4442ee00c5b1c
felipe-u <felipe_uv@outlook.com>
2024-10-07 15:17:00 -0500

    Creación de componentes faltantes

---
+++ finapp-frontend/src/app/features/account/account.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './account.component.html',
+  styleUrl: './account.component.css'
+})
+export class AccountComponent {
+  private router = inject(Router);
+
+  goToSettings() {
+    this.router.navigate(['account', 'settings'])
+  }
+
+  goBack() {
+    this.router.navigate(['']);
+  }
+}
---
+++ finapp-frontend/src/app/features/account/settings/settings.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.css'
+})
+export class SettingsComponent {
+  private router = inject(Router);
+
+  goToProfile() {
+    this.router.navigate(['account', 'profile']);
+  }
+
+  goBack() {
+    this.router.navigate(['']);
+  }
+}
