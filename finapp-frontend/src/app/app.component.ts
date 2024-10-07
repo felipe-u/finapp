f83a5782fe90b653c819cbfdd116788e1ae25f3d
felipe-u <felipe_uv@outlook.com>
2024-10-07 15:18:52 -0500

    Construccion del Enrutamiento

--- finapp-frontend/src/app/app.component.ts
+++ finapp-frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { HeaderComponent } from "./shared/header/header.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent, FooterComponent],
+  imports: [HeaderComponent, NavbarComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ finapp-frontend/src/app/app.routes.ts
@@ -0,0 +1,60 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./features/home/home.component";
+import { ClientsComponent } from "./features/clients/clients.component";
+import { ReportsComponent } from "./features/reports/reports.component";
+import { NotFoundComponent } from "./shared/not-found/not-found.component";
+import { MainLayoutComponent } from "./layouts/main-layout/main-layout.component";
+import { AccountLayoutComponent } from "./layouts/account-layout/account-layout.component";
+import { AccountComponent } from "./features/account/account.component";
+import { SettingsComponent } from "./features/account/settings/settings.component";
+import { SupportComponent } from "./features/support/support.component";
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: '',
+                component: HomeComponent,
+                title: 'FinApp | Home'
+            },
+            {
+                path: 'clients',
+                component: ClientsComponent,
+                title: 'FinApp | Clients'
+            },
+            {
+                path: 'reports',
+                component: ReportsComponent,
+                title: 'FinApp | Reports'
+            },
+            {
+                path: 'support',
+                component: SupportComponent,
+                title: 'FinApp | Support'
+            }
+        ]
+    },
+    {
+        path: 'account',
+        component: AccountLayoutComponent,
+        children: [
+            {
+                path: 'profile',
+                component: AccountComponent,
+                title: 'Profile'
+            },
+            {
+                path: 'settings',
+                component: SettingsComponent,
+                title: 'Settings'
+            }
+        ]
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+        title: '404 | Not Found'
+    }
+]
