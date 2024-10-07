779901e292965412f9c92453510a1ac732dd0d38
felipe-u <felipe_uv@outlook.com>
2024-10-07 15:17:45 -0500

    Creación de layouts para organizar las vistas

---
+++ finapp-frontend/src/app/layouts/account-layout/account-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-account-layout',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './account-layout.component.html',
+  styleUrl: './account-layout.component.css'
+})
+export class AccountLayoutComponent {
+
+}
---
+++ finapp-frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/header/header.component";
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [HeaderComponent, NavbarComponent, FooterComponent, RouterOutlet],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+
+}
