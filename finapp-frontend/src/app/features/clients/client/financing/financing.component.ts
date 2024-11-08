64d010c3c2377d3c6995cedf7e59383f9c8445da
felipe-u <felipe_uv@outlook.com>
2024-11-07 22:26:27 -0500

    Creación del componente Financing

---
+++ finapp-frontend/src/app/features/clients/client/financing/financing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financing',
+  standalone: true,
+  imports: [],
+  templateUrl: './financing.component.html',
+  styleUrl: './financing.component.css'
+})
+export class FinancingComponent {
+
+}
