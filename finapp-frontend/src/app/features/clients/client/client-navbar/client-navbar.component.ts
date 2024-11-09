1b9693b1fe0c4afb64270aa64401b711ccf963c6
felipe-u <felipe_uv@outlook.com>
2024-11-08 20:51:34 -0500

    Cambio de colores y Refactor Navbar cliente

--- finapp-frontend/src/app/features/clients/client/client-navbar/client-navbar.component.ts
+++ finapp-frontend/src/app/features/clients/client/client-navbar/client-navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-client-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './client-navbar.component.html',
   styleUrl: './client-navbar.component.css'
 })
--- finapp-frontend/src/app/features/clients/client/client.component.ts
+++ finapp-frontend/src/app/features/clients/client/client.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { ClientNavbarComponent } from "./client-navbar/client-navbar.component";
 
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, ClientNavbarComponent],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
