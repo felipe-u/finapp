bc43b9e786defcec8ffe0d56603b127cd8e11b31
felipe-u <felipe_uv@outlook.com>
2025-01-19 17:21:55 -0500

    Pequeño refactor

--- finapp-frontend/src/app/app.component.ts
+++ finapp-frontend/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
-import { HeaderComponent } from "./shared/header/header.component";
-import { NavbarComponent } from "./shared/navbar/navbar.component";
-import { FooterComponent } from "./shared/footer/footer.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent, FooterComponent, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
