fe968b5ad2eff27077b1c5d80bf0ee3411a9ef22
felipe-u <felipe_uv@outlook.com>
2025-03-13 21:05:38 -0500

    Revert "Se refactorizan las rutas del Frontend"
    This reverts commit a1471d574798127f312ae952c66244697a2395a8.

--- finapp-frontend/src/app/app.config.ts
+++ finapp-frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { routes } from './core/routes/app.routes';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
