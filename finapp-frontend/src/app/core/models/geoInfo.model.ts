53f16102dbdec8651dae349769800b234388d2d3
felipe-u <felipe_uv@outlook.com>
2025-03-29 23:30:28 -0500

    Se ajustan estilos

--- finapp-frontend/src/app/core/models/geoInfo.model.ts
+++ finapp-frontend/src/app/core/models/geoInfo.model.ts
@@ -13,14 +13,3 @@ export class GeoInfo {
         public additionalInfo?: string,
     ) { }
 }
-
-// EJEMPLO DE USO
-// const address = new Address('Colombia', 'Antioquia', 'Medellín');
-// const location = new Location([-75.56359, 6.25184]); // Coordenadas de ejemplo
-// const googleMapsUrl = 'https://www.google.com/maps/place/Medell%C3%ADn,+Antioquia';
-// const images = [
-//   'https://example.com/photo1.jpg',
-//   'https://example.com/photo2.jpg',
-// ];
-
-// const geoInfo = new GeoInfo(address, location, googleMapsUrl, images);
