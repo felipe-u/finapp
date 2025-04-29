fbbd96c0a67a646e6506f7be00b155925bee062c
felipe-u <felipe_uv@outlook.com>
2025-04-28 22:40:39 -0500

    Se añade URL del servidor en Render. Se ajustan las opciones del build en el archivo angular.json. Por último se crea el archivo vercel.json para que converse con Vercel en el despliegue

--- finapp-frontend/src/environments/environment.prod.ts
+++ finapp-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    SERVER_URL: 'http://Localhost:3000',
+    SERVER_URL: 'https://finapp-iepd.onrender.com',
     CONFLUENCE_URL: 'https://felipeuv.atlassian.net/wiki/x/c4AC'
 }
