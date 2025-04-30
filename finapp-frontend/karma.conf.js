ba7cf4745ac1fd32a5352fdf5a4986c9b0331d6b
felipe-u <felipe_uv@outlook.com>
2025-04-29 19:26:47 -0500

    Se vincula la configuración de los tests del Frontend con el archivo karma.conf.js. En este archivo se añade la configuración de la elección del browser según el ambiente (local o de integración). Por último se añade la configuración del coverage para el análisis en Sonar

--- finapp-frontend/karma.conf.js
+++ finapp-frontend/karma.conf.js
@@ -15,21 +15,21 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [{ type: "html" }, { type: "lcov" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autowatch: true,
-    browsers: ["ChromeHeadless"],
+    browsers: [process.env.CI ? "ChromeHeadlessCI" : "Chrome"],
     customLaunchers: {
-      ChromeHeadless: {
+      ChromeHeadlessCI: {
         base: "ChromeHeadless",
         flags: ["--headless", "--no-sandbox"],
       },
     },
-    singleRun: false,
+    singleRun: !!process.env.CI,
     restartOnFileChange: true,
   });
 };
